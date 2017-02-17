99a77b1a85542f8c3b0feb06d02ef198a7456428
960208781 <960208781@qq.com>
2017-02-17 11:20:30 +0800

    Add files via upload

---
+++ PlayerWalk.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerWalk : StateMachineBehaviour {
+    public AudioClip walk;
+    AudioSource a;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (a==null) {
+            a = new GameObject().AddComponent<AudioSource>();
+        }
+        if (walk == null) {
+            walk = Resources.Load<AudioClip>("AudioSource/Effect/walk");
+        }
+        a.clip = walk;
+        a.loop = true;
+        a.pitch = PlayerHero.instance.mCurMoveSpeed;
+        a.Play();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        a.pitch = PlayerHero.instance.mCurMoveSpeed;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        a.Stop();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
